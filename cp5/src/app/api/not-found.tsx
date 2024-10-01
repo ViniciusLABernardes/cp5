ade6a320116655504028c3ea8d0f4faee022add8
lopeslyra10 <augustolopeslyra@gmail.com>
2024-10-01 20:47:10 -0300

    pasta public criada e imagem adicionada

--- cp5/src/app/api/not-found.tsx
+++ cp5/src/app/api/not-found.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 const NotFound = () => (
   <div>
     <h1>404 - Página Não Encontrada</h1>
-    <Image src="/404-image.png" alt="Ilustração de Erro" width={500} height={300} />
+    <Image src="../" alt="Ilustração de Erro" width={500} height={300} />
   </div>
 );
 export default NotFound;
