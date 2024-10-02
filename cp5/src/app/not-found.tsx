6b53414b4a20e24ae62e6646e0787818212d7d70
viniciuslabernardes <vinicius.l.a.bernardes@outlook.com>
2024-10-01 21:25:32 -0300

    colocando imagem de erro na pagina not-found

--- cp5/src/app/not-found.tsx
+++ cp5/src/app/not-found.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 const NotFound = () => (
   <div>
     <h1>404 - Página Não Encontrada</h1>
-    <Image src="/./../public/img/cp5/public/img/page-not-found-404.png" alt="Ilustração de Erro" width={500} height={300} />
+    <Image src="https://www.elegantthemes.com/blog/wp-content/uploads/2020/08/000-http-error-codes.png" alt="Ilustração de Erro" width={500} height={300} />
   </div>
 );
 export default NotFound;
--- cp5/src/app/pages/velikovsky-idea1.tsx
+++ cp5/src/app/pages/velikovsky-idea1.tsx
@@ -1,5 +1,5 @@
 export default function Velikovsky-idea1(){
     return(
-        
+
     )
 }
