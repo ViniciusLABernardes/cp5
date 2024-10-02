5c7a7aed6f6c6b1a70201a331b5862be14da55c0
viniciuslabernardes <vinicius.l.a.bernardes@outlook.com>
2024-10-01 21:13:21 -0300

    terminando requisicao da api

--- cp5/src/app/not-found.tsx
+++ cp5/src/app/not-found.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 const NotFound = () => (
   <div>
     <h1>404 - Página Não Encontrada</h1>
-    <Image src="../" alt="Ilustração de Erro" width={500} height={300} />
+    <Image src="/./../public/img/cp5/public/img/page-not-found-404.png" alt="Ilustração de Erro" width={500} height={300} />
   </div>
 );
 export default NotFound;
