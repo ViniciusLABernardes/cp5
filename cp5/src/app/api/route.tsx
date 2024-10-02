08f562efddb8a1898d549d48ed4353a0ea18d2ab
viniciuslabernardes <vinicius.l.a.bernardes@outlook.com>
2024-10-01 22:21:59 -0300

    centralizando imagem de erro

--- cp5/src/app/api/route.tsx
+++ cp5/src/app/api/route.tsx
@@ -1,5 +1,4 @@
-import { NextResponse } from "next/server";
-import {promises as fs} from "fs";
+
 
 export async function GET() {
     const nasaUrl = `https://api.nasa.gov/planetary/apod?api_key=R23wVPlEiWksGL1EsYMR84RxyVvQmC6zzXwZ8CT0&count=5`;
